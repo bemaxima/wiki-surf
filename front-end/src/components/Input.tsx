724f03f2f80af06c3ffbe52941dec9891bde8117
Ehsan <bemaxima@gmail.com>
2020-10-17 16:20:29 +0330

    Move input components to the presenter components directory.

---
+++ front-end/src/components/Input.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  width: 100%;
+  border: solid 1px #ccc;
+  border-radius: 5px;
+  line-height: 2rem;
+  padding: 5px 10px;
+  box-sizing: border-box;
+`;
+
+export default Input;
