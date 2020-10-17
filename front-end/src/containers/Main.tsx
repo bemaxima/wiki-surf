e52e6fdebe735ea5db53a4d108c602d4524b9b75
Ehsan <bemaxima@gmail.com>
2020-10-17 17:35:24 +0330

    Set focus on input on start-up.

--- front-end/src/containers/Main.tsx
+++ front-end/src/containers/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Input from '../components/Input';
 import WikiArticle from './WikiArticle';
 import { useDebounce } from '../hooks/useDebounce';
@@ -6,10 +6,19 @@ import { useDebounce } from '../hooks/useDebounce';
 const Main: React.FC = () => {
   const [keyword, setKeyword] = useState('');
   const page = useDebounce(keyword, 500);
+  const input = useRef<HTMLInputElement>(null);
+
+  useEffect(
+    () => {
+      input.current?.focus();
+    },
+    []
+  )
 
   return (
     <>
       <Input
+        ref={input}
         value={keyword}
         onChange={e => setKeyword(e.target.value)}
         placeholder='Enter the page title ...'
